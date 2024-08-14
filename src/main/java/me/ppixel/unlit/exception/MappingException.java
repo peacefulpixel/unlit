643e99fac25b6b194503f78cd00f4e71570adfdc
Oleg Sokolov <me@vorp.dev>
2024-08-14 13:09:47 +0300

    Exception docs

--- src/main/java/me/ppixel/unlit/exception/MappingException.java
+++ src/main/java/me/ppixel/unlit/exception/MappingException.java
@@ -1,6 +1,17 @@
 package me.ppixel.unlit.exception;
 
-// TODO: Doc
+/**
+ * Component field mapping exception.<br>
+ * This exception may happen during mapping of internal field of your MappedComponent (e.g. with @Id or @MarkupField).
+ * It's being caused by multiple different reasons, including:<br>
+ *   - Wrong name of setter in the XML template (it's not exists, or not applicable with provided value)<br>
+ *   - Its parent doesn't implement {@link com.vaadin.flow.component.HasComponents} interface, so it can't be added as a
+ *     child.<br>
+ *   - Child with provided ID is not found.
+ * <br>
+ * All the exception constructors are directly froward to the RuntimeException ones
+ * @see <a href="https://github.com/peacefulpixel/unlit/tree/master>Official README</a>
+ */
 public class MappingException extends RuntimeException {
     public MappingException() {
     }
